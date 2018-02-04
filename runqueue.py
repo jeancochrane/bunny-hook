5f9ba78aca17f3379275ad5b76f228fdc5a2fb95
Jean Cochrane <jean@jeancochrane.com>
2018-02-04 13:58:10 -0600

    Add in missing secrets file and rename queue script

---
+++ runqueue.py
@@ -0,0 +1,6 @@
+from api.queue import Queue
+
+
+if __name__ == '__main__':
+    queue = Queue()
+    queue.run()
--- runworker.py
+++
@@ -1,6 +0,0 @@
-from api.worker import Worker
-
-
-if __name__ == '__main__':
-    worker = Worker()
-    worker.run()
