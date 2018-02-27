4b1641aaf6868d34fba8f8ef7abcc59955cc49ec
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2018-02-26 20:02:54 -0500

    Fix app tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
-from contextlib import contextmanager
 import json
+import sys
 
-from flask import appcontext_pushed, g
 from werkzeug.datastructures import Headers
 
 import env
+import test_secrets
+sys.modules['api.secrets'] = test_secrets
 import api
 from api.routes import get_hmac
-from test_secrets import TOKENS
 
 
 class TestAPI(TestCase):
@@ -20,42 +20,30 @@ class TestAPI(TestCase):
         '''
         api.app.testing = True
         cls.app = api.app.test_client()
-        cls.tokens = TOKENS
+        cls.tokens = test_secrets.TOKENS
 
         # Good and bad authentication credentials
-        cls.good_sig = get_hmac(cls.tokens[0])
+        cls.good_sig = get_hmac(cls.tokens['master'])
         cls.bad_sig = get_hmac('bogus token')
 
-    @contextmanager
-    def authenticate(self):
-        '''
-        Inject fake security tokens into the app context for testing.
-        '''
-        def handler(sender, **kwargs):
-            g.tokens = self.tokens
-
-        with appcontext_pushed.connected_to(handler, api.app):
-            yield
-
-    def test_successful_request(self):
-        '''
-        Test a successful request.
-        '''
-        post_data = json.dumps({
+        cls.good_data = json.dumps({
                 'ref': 'refs/head/master',
                 'repository': {
                     'name': 'test-repo'
                 }
             })
 
+    def test_successful_request(self):
+        '''
+        Test a successful request.
+        '''
         headers = Headers()
         headers.add('X-Hub-Signature', self.good_sig)
 
-        with self.authenticate():
-            post_request = self.app.post('/hooks/github/master',
-                                         content_type='application/json',
-                                         data=post_data,
-                                         headers=headers)
+        post_request = self.app.post('/hooks/github/master',
+                                     content_type='application/json',
+                                     data=self.good_data,
+                                     headers=headers)
 
         self.assertEqual(post_request.status_code, 202)
 
@@ -67,23 +55,16 @@ class TestAPI(TestCase):
         '''
         Test a bad request where the secret token doesn't authenticate.
         '''
-        post_data = json.dumps({
-                'ref': 'refs/head/master',
-                'repository': {
-                    'name': 'test-repo'
-                }
-            })
-
         headers = Headers()
         headers.add('X-Hub-Signature', self.bad_sig)
 
-        with self.authenticate():
-            post_request = self.app.post('/hooks/github/master',
-                                         content_type='application/json',
-                                         data=post_data,
-                                         headers=headers)
 
-        self.assertEqual(post_request.status_code, 401)
+        post_request = self.app.post('/hooks/github/master',
+                                     content_type='application/json',
+                                     data=self.good_data,
+                                     headers=headers)
+
+        self.assertEqual(post_request.status_code, 403)
 
         response = json.loads(post_request.data.decode('utf-8'))
         expected = 'Request signature failed to authenticate'
@@ -94,21 +75,18 @@ class TestAPI(TestCase):
         When the ref path from GitHub is different from the hook branch name,
         make sure that the app does nothing.
         '''
-        post_data = json.dumps({'ref': 'refs/heads/master'})
-
         headers = Headers()
         headers.add('X-Hub-Signature', self.good_sig)
 
-        with self.authenticate():
-            post_request = self.app.post('/hooks/github/deploy',
-                                         content_type='application/json',
-                                         data=post_data,
-                                         headers=headers)
+        post_request = self.app.post('/hooks/github/deploy',
+                                     content_type='application/json',
+                                     data=self.good_data,
+                                     headers=headers)
 
-        self.assertEqual(post_request.status_code, 400)
+        self.assertEqual(post_request.status_code, 403)
 
         response = json.loads(post_request.data.decode('utf-8'))
-        msg = 'Skipping build for unregistered branch "refs/heads/master"'
+        msg = 'No token configured for deploy'
         self.assertEqual(response.get('status'), msg)
 
     def test_no_ref(self):
@@ -120,11 +98,10 @@ class TestAPI(TestCase):
         headers = Headers()
         headers.add('X-Hub-Signature', self.good_sig)
 
-        with self.authenticate():
-            post_request = self.app.post('/hooks/github/deploy',
-                                         content_type='application/json',
-                                         data=post_data,
-                                         headers=headers)
+        post_request = self.app.post('/hooks/github/master',
+                                     content_type='application/json',
+                                     data=post_data,
+                                     headers=headers)
 
         self.assertEqual(post_request.status_code, 400)
 
